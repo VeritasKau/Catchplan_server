79b481eac08f18f051ef4f53141bd273692b9b14
seongjju <jeongsj1226@naver.com>
2023-10-29 01:53:09 +0900

    31

--- src/main/java/Sanhak/wakeUp/team/dto/MemberInfoUpdateRequest.java
+++ src/main/java/Sanhak/wakeUp/team/dto/MemberInfoUpdateRequest.java
@@ -1,23 +1,23 @@
-//package Sanhak.wakeUp.team.dto;
-//
-//
-//import jakarta.validation.constraints.NotBlank;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//@Getter @Setter
-//public class MemberInfoUpdateRequest {
-//
-//    @NotBlank(message = "이름은 필수입니다.")
-//    private String name;    // 이름
-//
-//    @NotBlank(message = "성별은 필수입니다.")
-//    private String sex;     // 성별
-//
-//    @NotBlank(message = "장르는 필수입니다.")
-//    private String genre;   // 장르
-//
-//    @NotBlank(message = "MBTI는 필수입니다.")
-//    private String mbti;    // MBTI
-//
-//}
+package Sanhak.wakeUp.team.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MemberInfoUpdateRequest {
+
+    @NotBlank(message = "이름은 필수입니다.")
+    private String name;    // 이름
+
+    @NotBlank(message = "성별은 필수입니다.")
+    private String sex;     // 성별
+
+    @NotBlank(message = "장르는 필수입니다.")
+    private String genre;   // 장르
+
+    @NotBlank(message = "MBTI는 필수입니다.")
+    private String mbti;    // MBTI
+
+}
