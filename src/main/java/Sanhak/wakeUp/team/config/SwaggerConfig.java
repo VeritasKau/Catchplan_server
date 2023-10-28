38e3562bbccac1003dfd7e23f6d5dac2fd6cad3f
seongjju <jeongsj1226@naver.com>
2023-10-28 23:56:23 +0900

    16

--- src/main/java/Sanhak/wakeUp/team/config/SwaggerConfig.java
+++ src/main/java/Sanhak/wakeUp/team/config/SwaggerConfig.java
@@ -1,28 +1,28 @@
-//package Sanhak.wakeUp.team.config;
-//
-//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-//import io.swagger.v3.oas.annotations.info.Info;
-//import lombok.RequiredArgsConstructor;
-//import org.springdoc.core.models.GroupedOpenApi;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@OpenAPIDefinition(
-//        info = @Info(title = "sanhak API 명세서",
-//                description = "sanhak",
-//                version = "v2")
-//)
-//@RequiredArgsConstructor
-//@Configuration
-//public class SwaggerConfig {
-//    @Bean
-//    public GroupedOpenApi Home() {
-//        String[] paths = {"/user/**"};
-//
-//        return GroupedOpenApi.builder()
-//                .group("회원관리 page")
-//                .pathsToMatch(paths)
-//                .build();
-//    }
-//
-//}
+package Sanhak.wakeUp.team.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "sanhak API 명세서",
+                description = "sanhak",
+                version = "v2")
+)
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi Home() {
+        String[] paths = {"/user/**"};
+
+        return GroupedOpenApi.builder()
+                .group("회원관리 page")
+                .pathsToMatch(paths)
+                .build();
+    }
+
+}
