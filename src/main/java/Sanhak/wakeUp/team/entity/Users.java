7cdaa3ce5ca8c5c6cfbcec15573acdf59888147c
seongjju <jeongsj1226@naver.com>
2023-10-29 00:20:47 +0900

    20

--- src/main/java/Sanhak/wakeUp/team/entity/Users.java
+++ src/main/java/Sanhak/wakeUp/team/entity/Users.java
@@ -1,87 +1,31 @@
-package Sanhak.wakeUp.team.entity;
-
-import jakarta.persistence.*;
-import lombok.*;
-
-@ToString
-@Getter
-@Builder
-@AllArgsConstructor
-@Entity
-@Setter
-@NoArgsConstructor
-@Table(name="user")
-public class Users {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String email;
-    private String password;
-    private Integer age;
-
-
-    public Users(String name, String email, String password, Integer age) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.age = age;
-    }
-
-
-//    public static void setName(String name) {
-//        this.name = name;
-//    }
-//    private void setEmail(String email) {
-//        this.email = email;
-//    }
-//    private void setAge(Integer age) {
-//        this.age = age;
-//    }
-//    private void setPassword(String password) {
-//        this.password = password;
-//    }
-}
-
-
-
+//package Sanhak.wakeUp.team.entity;
 //
 //import jakarta.persistence.*;
-//import lombok.Builder;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
+//import lombok.*;
 //
-//import javax.management.relation.Role;
-//
-//@Entity
+//@ToString
 //@Getter
+//@Builder
+//@AllArgsConstructor
+//@Entity
+//@Setter
 //@NoArgsConstructor
+//@Table(name="user")
 //public class Users {
-//    private String email;
-//    private String phone;
 //    @Id
 //    @GeneratedValue(strategy = GenerationType.IDENTITY)
 //    private Long id;
 //    private String name;
+//    private String email;
 //    private String password;
-//    @Enumerated(EnumType.STRING)
-//    private Role role;
+//    private Integer age;
 //
-//    @Builder
 //
-//    public Users(Long id, String name,String email,String phone, String password, Role role) {
-//        this.id = id;
+//    public Users(String name, String email, String password, Integer age) {
 //        this.name = name;
-//        this.email=email;
-//        this.phone=phone;
+//        this.email = email;
 //        this.password = password;
-//        this.role = role;
+//        this.age = age;
 //    }
-//    public Users update(String name){
-//        this.name=name;
-//        return this;
-//    }
-//    public String getRoleKey(){
-//        return this.role.getKey();
-//    }
+//
 //}
