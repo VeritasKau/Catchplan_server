29bb3e82951ac19e9473d3a6f1907026f793a753
seongjju <jeongsj1226@naver.com>
2023-10-29 11:37:36 +0900

    40

--- src/main/java/Sanhak/wakeUp/team/TeamApplication.java
+++ src/main/java/Sanhak/wakeUp/team/TeamApplication.java
@@ -7,8 +7,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 //밑에 2개 추가
-//@EnableJpaAuditing
-//@ComponentScan(basePackages = "Sanhak.wakeUp")
+@EnableJpaAuditing
+@ComponentScan(basePackages = "Sanhak.wakeUp")
 public class TeamApplication {
 
 	public static void main(String[] args) {
