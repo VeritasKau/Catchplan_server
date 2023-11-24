665d37c0966f9bfacfe62c9304462ffa66a1eccc
seongjju <jeongsj1226@naver.com>
2023-11-24 19:41:33 +0900

    rollback

--- src/main/java/Sanhak/wakeUp/team/small_event/controller/SmallEventController.java
+++ src/main/java/Sanhak/wakeUp/team/small_event/controller/SmallEventController.java
@@ -1,12 +1,9 @@
 package Sanhak.wakeUp.team.small_event.controller;
 
+import Sanhak.wakeUp.team.event.dto.EventResponse;
 import Sanhak.wakeUp.team.small_event.dto.SmallEventRequest;
 import Sanhak.wakeUp.team.small_event.dto.SmallEventResponse;
 import Sanhak.wakeUp.team.small_event.service.SmallEventService;
-import Sanhak.wakeUp.team.global.exception.TokenNotFoundException;
-import Sanhak.wakeUp.team.global.exception.TokenValidationException;
-import Sanhak.wakeUp.team.global.utils.valid.TokenValidator;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,83 +16,75 @@ import java.util.List;
 @RequestMapping("/api/small_events")
 public class SmallEventController {
     private final SmallEventService smallEventService;
-    private final TokenValidator tokenValidator;
 
+    //새로운 small event 생성 api
     @PostMapping("/create")
-    public ResponseEntity<String> createSmallEvent(HttpServletRequest request, @RequestBody SmallEventRequest smallEventRequest) {
-        validateToken(request.getHeader("Authorization"));
+    public ResponseEntity<String> createSmallEvent(@RequestBody SmallEventRequest smallEventRequest){
         try {
             smallEventService.createSmallEvent(smallEventRequest);
             return new ResponseEntity<>("OK", HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>("Error", HttpStatus.INTERNAL_SERVER_ERROR);
+        }catch (Exception e){
+            return new ResponseEntity<>("Error",HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
+    //모든 small event 조회 api
     @GetMapping("")
-    public List<SmallEventResponse> getAllSmallEvents(HttpServletRequest request) {
-        validateToken(request.getHeader("Authorization"));
+    public List<SmallEventResponse> getAllSmallEvents(){
         return smallEventService.getAllSmallEvents();
     }
 
+    //small event 제목으로 가져오기
     @GetMapping("/text")
-    public List<SmallEventResponse> getSmallEventByText(HttpServletRequest request, @RequestParam("text") String text) {
-        validateToken(request.getHeader("Authorization"));
+    public List<SmallEventResponse> getSmallEventByText(@RequestParam("text") String text){
         List<SmallEventResponse> smallEventByText = smallEventService.getSmallEventByText(text);
-        if (smallEventByText != null) {
+        if(smallEventByText !=null){
             return smallEventByText;
-        } else {
+        }else{
             return null;
         }
     }
 
+    //small event 1개가져오기
     @GetMapping("/id")
-    public SmallEventResponse getSmallEventById(HttpServletRequest request, @RequestParam("id") Long id) {
-        validateToken(request.getHeader("Authorization"));
-        SmallEventResponse getSmallEvent = smallEventService.getSmallEventById(id);
-        if (getSmallEvent != null) {
+    public SmallEventResponse getSmallEventById(@RequestParam("id") Long id){
+        SmallEventResponse getSmallEvent=smallEventService.getSmallEventById(id);
+        if(getSmallEvent !=null){
             return getSmallEvent;
-        } else {
+        }else{
             return null;
         }
     }
 
+    //작성한 small event 업데이트
     @PutMapping("/{id}")
-    public ResponseEntity<String> editSmallEvent(HttpServletRequest request, @RequestBody SmallEventRequest smallEventRequest, @PathVariable("id") Long id) {
-        validateToken(request.getHeader("Authorization"));
+    public ResponseEntity<String> editSmallEvent(@RequestBody SmallEventRequest smallEventRequest,@PathVariable("id")Long id){
         try {
             SmallEventResponse editSmallEvent = smallEventService.editSmallEvent(smallEventRequest, id);
             if (editSmallEvent != null) {
                 return new ResponseEntity<>("OK", HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>("Not Found", HttpStatus.NOT_FOUND);
+            }else{
+                return new ResponseEntity<>("Not Found",HttpStatus.NOT_FOUND);
             }
-        } catch (Exception e) {
-            return new ResponseEntity<>("Error", HttpStatus.INTERNAL_SERVER_ERROR);
+        }catch (Exception e){
+            return new ResponseEntity<>("Error",HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
+
+    //작성한 small event삭제
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteSmallEvent(HttpServletRequest request, @PathVariable("id") Long id) {
-        validateToken(request.getHeader("Authorization"));
+    public ResponseEntity<String> deleteSmallEvent(@PathVariable("id")Long id){
         try {
             SmallEventResponse smallEventResponse = smallEventService.deleteSmallEvent(id);
-            if (smallEventResponse != null) {
-                return new ResponseEntity<>("OK", HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>("Not Found", HttpStatus.NOT_FOUND);
+            if(smallEventResponse !=null){
+                return new ResponseEntity<>("OK",HttpStatus.OK);
             }
-        } catch (Exception e) {
-            return new ResponseEntity<>("Error", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    private void validateToken(String authorizationHeader) {
-        try {
-            // JWT 토큰 검증
-            tokenValidator.validateToken(authorizationHeader.replace("Bearer ", ""));
-        } catch (TokenNotFoundException | TokenValidationException e) {
-            throw e;
+            else{
+                return new ResponseEntity<>("Not Found",HttpStatus.NOT_FOUND);
+            }
+        }catch (Exception e){
+            return new ResponseEntity<>("Error",HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
